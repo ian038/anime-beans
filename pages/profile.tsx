e282d556cdecf7d18ad6550673ed7d98e5c89afe
Ian Phua <ianphua@hotmail.com>
2021-08-11 14:33:23 -0400

    Modified Profile page

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,31 +1,18 @@
 import { Auth, Typography, Button } from '@supabase/ui'
 const { Text } = Typography
-import { supabase } from '../api'
 import React from 'react'
+import Layout from '../components/Layout'
 
 const Profile = (props) => {
-    const { user } = Auth.useUser()
-    if (user) {
-        return (
-            <>
-                <Text>Signed In: {user.email}</Text>
-                <Button block onClick={() => props.supabaseClient.auth.signOut()}>
-                    Sign Out
-                </Button>
-            </>
-        )
-    }
-    return props.children
-}
-
-const AuthProfile: React.FC = () => {
     return (
-        <Auth.UserContextProvider supabaseClient={supabase}>
-            <Profile supabaseClient={supabase}>
-                <Auth supabaseClient={supabase} />
-            </Profile>
-        </Auth.UserContextProvider>
+        <Layout>
+            <h1>Hello, you are authenticated! This is a protected page.</h1>
+            {/* <Text>Signed In: {user.email}</Text> */}
+            {/* <Button block onClick={() => props.supabaseClient.auth.signOut()}>
+                Sign Out
+            </Button> */}
+        </Layout>
     )
 }
 
-export default AuthProfile
+export default Profile
